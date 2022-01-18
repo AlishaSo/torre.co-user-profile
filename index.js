1e0959e15a53ded9124eeb76b6d76d2f9603465d
Alisha Soriano <sorianoalisha@gmail.com>
2022-01-18 17:18:09 -0500

    Add functionality to get profile image and user's name

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const profileImgEl = document.querySelector('.profile-img');
 const userNameEl = document.querySelector('.user-name');
+const masteredSkillsDiv = document.querySelector('.mastered-skills');
+const expertSkillsDiv = document.querySelector('.expert-skills');
+const proficientSkillsDiv = document.querySelector('.proficient-skills');
+const noviceSkillsDiv = document.querySelector('.novice-skills');
+const interestedSkillsDiv = document.querySelector('.interested-in-skills');
+
+function displayUserSkills(userInfo) {
+  profileImgEl.innerHTML = `<img src='${userInfo.pictureThumbnail}' alt='${userInfo.name}\'s profile picture'/>`;
+  userNameEl.textContent = `${userInfo.name}`;
+
+  let masteredSkillsList = userInfo.strengths.filter(strength => strength.proficiency = 'master');
+  console.log(masteredSkillsList)
+}
 
 fetch(' https://bio.torre.co/api/bios/sorianoalisha')
 .then(response => response.json())
 .then(userInfo => {
-  console.log(userInfo.name)
-  profileImgEl.innerHTML = `<img src='${userInfo.pictureThumbnail}' alt='${userInfo.name}\'s profile picture'/>`;
-  userNameEl.textContent = `${userInfo.name}`;
+  // console.log(userInfo.name)
+  displayUserSkills(userInfo)
+
 })
 .catch(error => console.error(error));
