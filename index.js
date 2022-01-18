c6938d2dffff1adff84b61897052c7241ca81fb6
Alisha Soriano <sorianoalisha@gmail.com>
2022-01-18 17:36:03 -0500

    Change method name for master skills; add check for other skill levels

--- index.js
+++ index.js
@@ -6,33 +6,49 @@ const proficientSkillsDiv = document.querySelector('.proficient-skills');
 const noviceSkillsDiv = document.querySelector('.novice-skills');
 const interestedSkillsDiv = document.querySelector('.interested-in-skills');
 
+function removeSpace(string) {
+  if(/\s/g.test(string)) {
+    string = string.split(/[ ]+/).join('-')
+  }
+  // console.log(string)
+  return string;
+}
+
 function getUserSkills(userInfo) {
   let masteredSkillsList = [];
-  profileImgEl.innerHTML = `<img src='${userInfo.person.pictureThumbnail}' alt='${userInfo.name}      \'s profile picture'/>`;
+  profileImgEl.innerHTML = `<img src='${userInfo.person.pictureThumbnail}' alt='${userInfo.name}\'s profile picture'/>`;
   userNameEl.textContent = `${userInfo.person.name}`;
 
 
-userInfo.strengths.forEach(strength => {
+  userInfo.strengths.forEach(strength => {
     if(strength.proficiency === 'master')
-      masteredSkillsList.push(strength.name)
+      masteredSkillsList.push(strength.name);
+    else if(strength.proficiency === 'expert')
+      expertSkillsList.push(strength.name);
+    else if(strength.proficiency === 'proficient')
+      proficientSkillsList.push(strength.name);
+    else if(strength.proficiency === 'novice')
+      noviceSkillsList.push(strength.name);
+    else if(strength.proficiency === 'no-experience-interested')
+      interestedSkillsList.push(strength.name);
 });
 
-getMasterSkillsHtml(masteredSkillsList)
-//   console.log(masteredSkillsList)
+creatMasterSkillsButtons(masteredSkillsList);
 }
 
-function getMasterSkillsHtml(masterSkillsArr) {
+function creatMasterSkillsButtons(masterSkillsArr) {
   if(masterSkillsArr) {
       masterSkillsArr.forEach(skill => {
-          // console.log(skill)
-          let skillBtn = document.createElement('button');
-          skillBtn.setAttribute('id', skill);
-          skillBtn.classList.add(`${skill}-btn`,'btn')
-          // console.log(skillBtn)
+        let skillNoSpace = removeSpace(skill).toLowerCase();
+        let skillBtn = document.createElement('button');
+        skillBtn.textContent = skill;
+        skillBtn.setAttribute('id', skillNoSpace);
+        skillBtn.classList.add('master-skill-btn','btn');
+        masteredSkillsDiv.appendChild(skillBtn);
       })
   }
   else {
-      masteredSkillsDiv.style.display = 'none';
+    masteredSkillsDiv.style.display = 'none';
   }
 }
 
